a76f86c84c87f9d7b62b9bb48ec860fbdaf36521
Felipe Pimentel de Aguiar <felipe.aguiar@philips.com>
2018-03-26 17:48:17 -0300

    feat: basic project structure

---
+++ src/components/Clock/Clock.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div><strong>Clock</strong></div>
+		);
+	}
+}
+
+export default Clock;
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import Clock from './components/Clock/Clock';
+
+render(<Clock />, document.getElementById('root'));
