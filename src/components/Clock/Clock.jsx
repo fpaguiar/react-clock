8ca6c4c09b480f61ef24e944569b965afc1f6a8b
Felipe Pimentel de Aguiar <felipe.pimentel.aguiar@gmail.com>
2018-03-29 23:57:58 -0300

    style: adjust spaces to tabs

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -2,23 +2,31 @@ import * as React from 'react';
 import moment from 'moment';
 import { updateTime } from './../../actions/actions';
 import { connect } from 'react-redux';
+import ClockOptions from './components/ClockOptions/ClockOptions';
 
 class Clock extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.intervalID = 0;
 	}
 
 	_getTime() {
-		return moment().format('h:mm:ss');
+		return moment().format('hh:mm:ss');
 	}
 
 	componentDidMount() {
-		setInterval(() => this.props.updateTime(this._getTime()), 1000);
+		this.intervalID = setInterval(() => this.props.updateTime(this._getTime()), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.intervalID);
 	}
 
 	render() {
 		return (
 			<div className="clock">
+				<ClockOptions />
 				<div className="content">{this.props.time}</div>
 			</div>
 		);
